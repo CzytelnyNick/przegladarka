133d864ad8017518fdb7649f4dfde4feeee95def
Damian <odrowazd@gmail.com>
2023-12-11 18:02:48 +0100

    add prev and next arrow

--- przegladarka.py
+++ przegladarka.py
@@ -7,16 +7,22 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QLabel,
     QFileDialog,
+    QVBoxLayout
 )
+from PIL import Image
 from PyQt6 import QtCore, uic
 from PyQt6.QtGui import QPixmap, QAction, QIcon
-import sys
+import sys, os
 import subprocess
-
-
+from urllib.parse import urlparse
+imageExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"]
 class Login(QMainWindow):
     def __init__(self):
-        dialog = ""
+        global dialog
+        
+        global img
+        dialog = ["", ""]
+        img = QPixmap("selectPhoto.jpg")
         
         super().__init__()
         self.ui = uic.loadUi("przegladarka.ui", self)
@@ -26,24 +32,110 @@ class Login(QMainWindow):
         button2 = QAction(QIcon("larrow.svg"), "2Your button", self)
         button3 = QAction(QIcon("rarrow.svg"), "3Your button", self)
         button1.triggered.connect(self.openFileExplorer)
+        button2.triggered.connect(self.prevImg)
+        button3.triggered.connect(self.nextImg)
         self.addToolBar(toolbar)
         toolbar.addAction(button1)
         toolbar.addAction(button2)
         toolbar.addAction(button3)
-        imageContainer = QLabel()
-        img = QPixmap(dialog)
-        imageContainer.setPixmap(img)
-        print(dialog)
+        self.imageContainer = QLabel()
         
+        
+        self.imageContainer.setPixmap(img)
+        layout = QVBoxLayout()
+        layout.addWidget(self.imageContainer)
+
+        # Tworzenie widgetu z layout'em i ustawienie go jako centralnego widgetu QMainWindow
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+        # self.imageContainer.setGeometry(200, 200, 200, 200)
         
         self.show()
+    def nextImg(self):
+        global imageExtensions
+        global dialog
+        print(dialog)
+        parsed_url = urlparse(dialog[0])
+        print(parsed_url)
+        # Uzyskaj ostatni segment ścieżki, który powinien być nazwą pliku
+        filename = os.path.basename(parsed_url.path)
+        print()
+        files = os.listdir("./galeria")
+        print((os.path.splitext(dialog[0]))[1], (os.path.splitext(dialog[0]))[1] in imageExtensions)
+        if filename in files and (os.path.splitext(dialog[0]))[1] in imageExtensions:
+            index = files.index(filename)
+            print(index)
+            length = len(files)-2
+            print(length)
+            if index <= length:
+                dialog = [dialog[0].replace(filename, files[index+1]), "JPG (*.jpg);;BMP (*.bmp);;CUR (*.cur);;GIF (*.gif);;ICNS (*.icns);;ICO (*.ico);;JPEG (*.jpeg);;PBM (*.pbm);;PGM (*.pgm);;PNG (*.png);;PPM (*.ppm);;SVG (*.svg);;SVGZ (*.svgz);;TGA (*.tga);;TIF (*.tif);;TIFF (*.tiff);;WBMP (*.wbmp);;WEBP (*.webp);;XBM (*.xbm);;XPM (*.xpm)"]
+            else:
+                dialog = [dialog[0].replace(filename, files[0]), "JPG (*.jpg);;BMP (*.bmp);;CUR (*.cur);;GIF (*.gif);;ICNS (*.icns);;ICO (*.ico);;JPEG (*.jpeg);;PBM (*.pbm);;PGM (*.pgm);;PNG (*.png);;PPM (*.ppm);;SVG (*.svg);;SVGZ (*.svgz);;TGA (*.tga);;TIF (*.tif);;TIFF (*.tiff);;WBMP (*.wbmp);;WEBP (*.webp);;XBM (*.xbm);;XPM (*.xpm)"]
+            print(dialog)
+            file_path = dialog[0]
+            
+
+            img = QPixmap(file_path)
+            file_size = self.get_image_size(file_path)
+            print(file_size)
+            self.setFixedSize(file_size[0]+100,file_size[1]+100)
+            self.imageContainer.setPixmap(img)
+    def prevImg(self):
+        global imageExtensions
+        global dialog
+        print(dialog)
+        parsed_url = urlparse(dialog[0])
+        print(parsed_url)
+        # Uzyskaj ostatni segment ścieżki, który powinien być nazwą pliku
+        filename = os.path.basename(parsed_url.path)
+        print()
+        files = os.listdir("./galeria")
+        print((os.path.splitext(filename))[1], (os.path.splitext(filename))[1] in imageExtensions)
+        if filename in files and (os.path.splitext(filename))[1] in imageExtensions:
+            index = files.index(filename)
+            print(index)
+            length = len(files)-2
+            print(length)
+            if index <= length:
+                dialog = [dialog[0].replace(filename, files[index-1]), "JPG (*.jpg);;BMP (*.bmp);;CUR (*.cur);;GIF (*.gif);;ICNS (*.icns);;ICO (*.ico);;JPEG (*.jpeg);;PBM (*.pbm);;PGM (*.pgm);;PNG (*.png);;PPM (*.ppm);;SVG (*.svg);;SVGZ (*.svgz);;TGA (*.tga);;TIF (*.tif);;TIFF (*.tiff);;WBMP (*.wbmp);;WEBP (*.webp);;XBM (*.xbm);;XPM (*.xpm)"]
+            else:
+                dialog = [dialog[0].replace(filename, files[0]), "JPG (*.jpg);;BMP (*.bmp);;CUR (*.cur);;GIF (*.gif);;ICNS (*.icns);;ICO (*.ico);;JPEG (*.jpeg);;PBM (*.pbm);;PGM (*.pgm);;PNG (*.png);;PPM (*.ppm);;SVG (*.svg);;SVGZ (*.svgz);;TGA (*.tga);;TIF (*.tif);;TIFF (*.tiff);;WBMP (*.wbmp);;WEBP (*.webp);;XBM (*.xbm);;XPM (*.xpm)"]
+            print(dialog)
+            file_path = dialog[0]
+            
+
+            img = QPixmap(file_path)
+            file_size = self.get_image_size(file_path)
+            print(file_size)
+            self.setFixedSize(file_size[0]+100,file_size[1]+100)
+            self.imageContainer.setPixmap(img)
+    def get_image_size(self, file_path):
+            try:
+                if (os.path.splitext(file_path))[1] in imageExtensions:
+                    with Image.open(file_path) as img:
+                        width, height = img.size
+                        return width, height
+            except Exception as e:
+                print(f"Błąd: {e}")
+                return None
 
     def openFileExplorer(self):
         global dialog
-        dialog = QFileDialog.getOpenFileName(directory="C:\\Users\\4TP2\\Desktop\\python\\designer\\galeria")
-        print(dialog[0])
-        dialog = dialog[0]
-
+        global imageExtensions
+        
+        dialog = QFileDialog.getOpenFileName(directory="./galeria", filter="JPG (*.jpg);;BMP (*.bmp);;CUR (*.cur);;GIF (*.gif);;ICNS (*.icns);;ICO (*.ico);;JPEG (*.jpeg);;PBM (*.pbm);;PGM (*.pgm);;PNG (*.png);;PPM (*.ppm);;SVG (*.svg);;SVGZ (*.svgz);;TGA (*.tga);;TIF (*.tif);;TIFF (*.tiff);;WBMP (*.wbmp);;WEBP (*.webp);;XBM (*.xbm);;XPM (*.xpm)")
+        print(dialog)
+        if (os.path.splitext(dialog[0]))[1] in imageExtensions:
+            file_path = dialog[0]
+            
+            img = QPixmap(file_path)
+            print(file_path)
+            file_size = self.get_image_size(file_path)
+            print(file_size)
+            self.setFixedSize(file_size[0]+100,file_size[1]+100)
+            # Use self.imageContainer instead of local imageContainer
+            self.imageContainer.setPixmap(img)
 
 app = QApplication(sys.argv)
 logowanie = Login()
